8011e165c0df842309cd5f924624284f5d0755b0
TJVasquezSchool <tvasquez@cpp.edu>
2025-11-19 13:26:46 -0800

    Implemented safe to place function

--- board_utils.asm
+++ board_utils.asm
@@ -13,6 +13,16 @@
 
 .end_macro
 
-.macro safe_to_place(%x, %y, %num) #A macro to check if a given number is safe to place in a specific position
+.globl safe_to_place
+#A function to check if a given number is safe to place in a specific position
+# $a0 - x
+# $a1 - y
+# $a2 - number
+safe_to_place: 
+	row_safe($a1, $a2)
+	col_safe($a0, $a2)
+	row_safe($a0, $a1, $a2)
+	and $v0, $t0, $t1
+	and $v0, $v0, $t2
+	jr $ra
 
-.end_macro
