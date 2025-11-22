1282a56f28782d7d31467d9442d1515eba13c2e1
TJVasquezSchool <tvasquez@cpp.edu>
2025-11-21 18:42:58 -0800

    Removed use of t4 register

--- board_utils.asm
+++ board_utils.asm
@@ -3,13 +3,13 @@
 .include "macros.asm"
 
 .macro row_body()
-	load_entry($t6, $t8, $t5) 
-	beq $t9, $t5, num_detected #if number in cell ($t5) equals the number to check ($t9) return 0
+	load_entry($t6, $t8, $t0) 
+	beq $t9, $t0, num_detected #if number in cell ($t0) equals the number to check ($t9) return 0
 .end_macro
 
 .macro col_body()
-	load_entry($t8, $t6, $t5) 
-	beq $t9, $t5, num_detected #if number in cell ($t5) equals the number to check ($t9) return 0
+	load_entry($t8, $t6, $t0) 
+	beq $t9, $t0, num_detected #if number in cell ($t0) equals the number to check ($t9) return 0
 .end_macro
 
 .macro box_outer()
@@ -19,8 +19,8 @@
 .macro box_inner()
 	add $t0, $t6, $t2
 	add $t1, $t5, $t3
-	load_entry($t0, $t1, $t4) 
-	beq $t9, $t4, num_detected #if number in cell ($t4) equals the number to check ($t9) return 0
+	load_entry($t0, $t1, $t0) 
+	beq $t9, $t0, num_detected #if number in cell ($t0) equals the number to check ($t9) return 0
 .end_macro
 
 .globl safe_to_place
@@ -42,10 +42,10 @@ safe_to_place:
 	
 	#check if box is safe
 	#calculate the start position of the current box
-	li $t2, 3
-	modulo($t7, $t2, $t6) #mod x and 3 into $t6
+	li $t5, 3
+	modulo($t7, $t5, $t6) #mod x and 3 into $t6
 	sub $t6, $t7, $t6 #$t6 = x - $t6 to get box start x
-	modulo($t8, $t2, $t5) #mod y and 3 into $t5
+	modulo($t8, $t5, $t5) #mod y and 3 into $t5
 	sub $t5, $t8, $t5 #$t5 = y - $t5 to get box start y
 	
 	for($t2, 0, 2, box_outer)
