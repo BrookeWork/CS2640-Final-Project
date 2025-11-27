93bc3f04ce434d1e446bcbd961d0215140d509dc
TJVasquezSchool <tvasquez@cpp.edu>
2025-11-26 23:02:47 -0800

    Moved sample generation to separate file

--- sudoku.asm
+++ sudoku.asm
@@ -1,17 +1,18 @@
 # Adam Khalil, Brooke Landry, Christian Parkinson, TJ Vasquez
 .include "macros.asm"
 .include "print_grid.asm"
-.include "board_utils.asm"
-.include "generate_puzzle.asm"
-.include "verify_grid.asm"
+.include "generate_sample_puzzle.asm"
+
 
 .data
 	.align 2
 	grid: .space 324
 	welcome_msg: .asciiz "Welcome to MIPS Sudoku!\n"
+	difficulty_prompt: .asciiz "Enter a target difficulty (1-5): "
 	rules_msg: .asciiz "Enter row and column (1-9), then a number (1-9) to place.\n"
 	play_prompt: .asciiz "Enter 1 to play, 0 to exit: "
 	invalid_choice_msg: .asciiz "Invalid choice, please try again.\n"
+	divider_msg: .asciiz "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 	row_prompt: .asciiz "Row (1-9): "
 	col_prompt: .asciiz "Column (1-9): "
 	num_prompt: .asciiz "Number (1-9): "
@@ -22,17 +23,14 @@
 	goodbye_msg: .asciiz "Goodbye!\n"
 
 .text
+.globl main
 main:
-	li $s0, 1
-	store_entry(1, 2, $s0)
-	store_entry(8, 0, $s0)
-	li $s0, 5
-	store_entry(5, 6, $s0)
-	store_entry(7, 2, $s0)
-	li $s0, 9
-	store_entry(6, 7, $s0)
-	store_entry(3, 3, $s0)
+	generate_sample_puzzle()
+	
 	print_grid()
 
 	li $v0, 10
 	syscall
+
+.include "verify_grid.asm"
+.include "board_utils.asm"
