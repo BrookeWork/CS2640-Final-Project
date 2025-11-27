4dccb25a0fac4b4069383414bdf1355d621347b5
TJVasquezSchool <tvasquez@cpp.edu>
2025-11-26 22:48:03 -0800

    Removed macros from safe to place

--- board_utils.asm
+++ board_utils.asm
@@ -1,43 +1,16 @@
 #Board utilities
-row_body:
-	load_entry($t5, $a1, $t0) 
-	beq $a2, $t0, num_detected #if number in cell ($t0) equals the number to check ($a2) return 0
-	add $t5, $t5, 1
-
-	ble $t5, 8, row_body
-	jr $ra
-
-col_body:
-	load_entry($a0, $t5, $t0) 
-	beq $a2, $t0, num_detected #if number in cell ($t0) equals the number to check ($a2) return 0
-	add $t5, $t5, 1
-	ble $t5, 8, col_body
-	jr $ra
-
-.macro box_inner(%index)
-	add $t1, $t5, $t2
-	add $t0, $t4, %index
-	load_entry($t1, $t0, $t0)
-	beq $a2, $t0, num_detected
-.end_macro
-
-box_outer:
-	#for_range($t3, 0, 2, box_inner)
-	box_inner(0)
-	box_inner(1)
-	box_inner(2)
-	add $t2, $t2, 1
-	ble $t2, 2, box_outer # jump back to box outer
-	jr $ra
-
-.globl safe_to_place
 #A function to check if a given number is safe to place in a specific position
 # $a0 - x
 # $a1 - y
 # $a2 - number
+# Returns:
+# $v0 - 1 if safe, 0 if not safe
 # uses regesters $t0 to $t5
+.globl safe_to_place
 safe_to_place:
-	move $t3, $ra
+	#store main return adress on stack
+	addi $sp, $sp, -4
+	sw   $ra, 0($sp)
 	
 	#check if row is safe
 	#for_range($t5, 0, 8, row_body)
@@ -61,9 +34,53 @@ safe_to_place:
 	li $t2, 0
 	jal box_outer
 	
-	li $v0, 1 #if none of the previous checks branched, return that it is safe
-	jr $t3
+	li $v0, 1 #if none of the previous checks branched, return 1 meaning that it is safe
+	
+	#load adress on stack pointer and return
+	lw   $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+
+row_body:
+	load_entry($t5, $a1, $t0) 
+	beq $a2, $t0, num_detected #if number in cell ($t0) equals the number to check ($a2) return 0
+	add $t5, $t5, 1
+
+	ble $t5, 8, row_body
+	jr $ra
+
+col_body:
+	load_entry($a0, $t5, $t0) 
+	beq $a2, $t0, num_detected #if number in cell ($t0) equals the number to check ($a2) return 0
+	add $t5, $t5, 1
+	
+	ble $t5, 8, col_body
+	jr $ra
+
+box_outer:
+	li $t3, 0
+
+box_inner_loop:
+	bge $t3, 3, box_inner_done
+	
+	add $t1, $t5, $t2 # t1 = t5 + outer index
+	add $t0, $t4, $t3 # t0 = t4 + inner index
+	
+	load_entry($t1, $t0, $t0)
+	
+	beq $a2, $t0, num_detected #if number in cell ($t0) equals the number to check ($a2) return 0
+
+	addi $t3, $t3, 1
+	j box_inner_loop
+
+box_inner_done:
+	addi $t2, $t2, 1
+	ble $t2, 2, box_outer
+	jr $ra
 
 num_detected:
-	li $v0, 0 #if any of the previous checks branched, return that it is not safe
-	jr $t3
+	li $v0, 0 #if any of the previous checks branched, return 0 meaning that it is not safe
+	#load adress on stack pointer and return
+	lw   $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
